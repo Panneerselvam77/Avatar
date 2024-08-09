338a50625a7b61de6b417c7d4fb012b1d6f9bd5e
Panneerselvam77 <panneerdeveloper77@gmail.com>
2024-08-09 08:34:54 +0530

    update

--- src/components/AvatarPicker.jsx
+++ src/components/AvatarPicker.jsx
@@ -13,16 +13,17 @@ import "./avatar.css";
 
 const DEFAULT_AVATAR_ID = 1;
 
-const Avatar = ({ source, label }) => {
+const Avatar = ({ source, label, onClick }) => {
   return (
     <div className="avatar">
-      <img src={source} alt={label} />
+      <img src={source} alt={label} onClick={onClick} />
     </div>
   );
 };
 const AvatarList = ({ avatars }) => {
+  // Visible / hidden
   return (
-    <div className="avatar-list">
+    <div className="avatar-list visible">
       {avatars.map((avatar) => (
         <Avatar key={avatar.id} {...avatar} />
       ))}
@@ -31,13 +32,20 @@ const AvatarList = ({ avatars }) => {
 };
 export default function AvatarPicker({ avatars }) {
   const [activeAvatarId, setActiveAvatarId] = useState(DEFAULT_AVATAR_ID);
+  const [showAvatarList, toggleAvatarListVisibility] = useState(false);
+
   const currentActiveAvatar = avatars.find(
     (avatar) => avatar.id === activeAvatarId
   );
   return (
     <div className="container">
-      <Avatar {...currentActiveAvatar} />
-      <AvatarList avatars={avatars} />
+      <Avatar
+        onClick={() => {
+          toggleAvatarListVisibility((prev) => !prev);
+        }}
+        {...currentActiveAvatar}
+      />
+      {showAvatarList ? <AvatarList avatars={avatars} /> : null}
     </div>
   );
 }
